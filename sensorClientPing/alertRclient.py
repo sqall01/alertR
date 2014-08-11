e7054dd4010486b3961ec765f4734fb3142f15f3
Andre Pawlowski <sqall@towel.hq.h4des.org>
2014-08-11 16:33:00 +0200

    initial commit of ping sensor client

---
+++ sensorClientPing/alertRclient.py
@@ -0,0 +1,268 @@
+#!/usr/bin/python2
+
+# written by sqall
+# twitter: https://twitter.com/sqall01
+# blog: http://blog.h4des.org
+# github: https://github.com/sqall01
+#
+# Licensed under the GNU Public License, version 2.
+
+import sys
+import os
+from lib import ServerCommunication, ConnectionWatchdog
+from lib import SMTPAlert
+from lib import PingWatchdogSensor, SensorExecuter
+import logging
+import time
+import ConfigParser
+import socket
+
+
+# this class is a global configuration class that holds 
+# values that are needed all over the client
+class GlobalData:
+
+	def __init__(self):
+
+		# version of the used client (and protocol)
+		self.version = 0.1
+
+		# interval in which a ping should be send when 
+		# no data was received/send
+		self.pingInterval = 30
+
+		# type of this node/client
+		self.nodeType = "sensor"
+
+		# path to the configuration file that holds the parameters
+		# that are registered at the server
+		self.registeredFile = os.path.dirname(os.path.abspath(__file__)) \
+			+ "/config/registered"
+
+		# this flags indicates if the client is already registered or not
+		self.registered = None
+
+		# path to the configuration file of the client
+		self.configFile = os.path.dirname(os.path.abspath(__file__)) \
+			+ "/config/config.conf"
+
+		# instance of the email alerting object
+		self.smtpAlert = None
+
+		# list of all sensors that are managed by this client
+		self.sensors = list()
+
+
+if __name__ == '__main__':
+
+	# generate object of the global needed data
+	globalData = GlobalData()
+
+	fileName = os.path.basename(__file__)
+
+	# parse config file
+	try:
+		config = ConfigParser.RawConfigParser(allow_no_value=False)
+
+		# get config file
+		config.read([globalData.configFile])
+
+		logfile = config.get("general", "logfile")
+		server = config.get("general", "server")
+		serverPort = config.getint("general", "serverPort")
+
+		# get server certificate file and check if it does exist
+		serverCertificate = os.path.abspath(
+			config.get("general", "serverCertificate"))
+		if os.path.exists(serverCertificate) is False:
+			raise ValueError("Server certificate does not exist.")
+
+		# get user credentials
+		username = config.get("general", "username")
+		password = config.get("general", "password")
+
+		# parse chosen log level
+		tempLoglevel = config.get("general", "loglevel")
+		tempLoglevel = tempLoglevel.upper()
+		if tempLoglevel == "DEBUG":
+			loglevel = logging.DEBUG
+		elif tempLoglevel == "INFO":
+			loglevel = logging.INFO
+		elif tempLoglevel == "WARNING":
+			loglevel = logging.WARNING
+		elif tempLoglevel == "ERROR":
+			loglevel = logging.ERROR
+		elif tempLoglevel == "CRITICAL":
+			loglevel = logging.CRITICAL
+		else:
+			raise ValueError("No valid log level in config file.")
+
+		# parse smtp options if activated
+		smtpActivated = config.getboolean("smtp", "smtpActivated")
+		if smtpActivated is True:
+			smtpServer = config.get("smtp", "server")
+			smtpPort = config.getint("smtp", "serverPort")
+			smtpFromAddr = config.get("smtp", "fromAddr")
+			smtpToAddr = config.get("smtp", "toAddr")
+
+		# parse all sensors
+		for section in config.sections():
+			if section.find("sensor") != -1:
+				sensor = PingWatchdogSensor()
+				sensor.id = config.getint(section, "id")
+				sensor.description = config.get(section, "description")
+				sensor.alertDelay = config.getint(section, "alertDelay")
+				sensor.alertLevel = config.getint(section, "alertLevel")
+				sensor.triggerAlert = config.getboolean(section,
+					"triggerAlert")
+				sensor.triggerAlways = config.getboolean(section,
+					"triggerAlways")
+				sensor.triggerState = config.getint(section, "triggerState")
+
+				# ctf watchdog specific options
+				sensor.timeout = config.getint(section, "timeout")
+				sensor.intervalToCheck = config.getint(section,
+					"intervalToCheck")
+				sensor.host = config.get(section, "host")
+				sensor.execute = config.get(section, "execute")
+
+				# check if description is empty
+				if len(sensor.description) == 0:
+					print "Description of sensor '%s' is empty." % section
+					sys.exit(1)
+
+				# check if the id of the sensor is unique
+				for registeredSensor in globalData.sensors:
+					if registeredSensor.id == sensor.id:
+						print "Id of sensor '%s'" % section \
+						+ "is already taken."
+						sys.exit(1)						
+
+				globalData.sensors.append(sensor)
+
+		# check if the client has already registered itself at the server
+		# with the same data
+		if os.path.exists(globalData.registeredFile):
+
+			# parse registered values
+			registeredConfig = ConfigParser.RawConfigParser(
+				allow_no_value=False)
+			registeredConfig.read([globalData.registeredFile])
+
+			hostname = registeredConfig.get("general", "hostname")
+			sensorCount = registeredConfig.getint("general", "sensorcount")
+
+			# check if the hostname or sensor count has changed
+			if (hostname == socket.gethostname()
+				and sensorCount == len(globalData.sensors)):
+
+				# check all sensors if their data has changed since the
+				# last registration at the server
+				for section in registeredConfig.sections():
+					if section.find("sensor") != -1:			
+
+						# get values from the file that are registered at
+						# the server
+						tempId = registeredConfig.getint(section, "id")
+						tempAlertDelay = registeredConfig.getint(section,
+							"alertDelay")
+						tempDescription = registeredConfig.get(section,
+							"description")
+						tempAlertLevel = registeredConfig.getint(section,
+							"alertLevel")
+						tempTriggerAlways = registeredConfig.getboolean(
+							section, "triggerAlways")
+
+						# find sensor with the same id parsed from the
+						# regular config file
+						tempSensor = None
+						for sensor in globalData.sensors:
+							if sensor.id == tempId:
+								tempSensor = sensor
+								break
+						if tempSensor == None:
+							globalData.registered = False
+							break
+
+						# check if the sensor data has changed since
+						# the last registration
+						if (tempAlertDelay != tempSensor.alertDelay
+							or tempDescription != tempSensor.description
+							or tempAlertLevel != tempSensor.alertLevel
+							or tempTriggerAlways != tempSensor.triggerAlways):
+							globalData.registered = False
+							break
+
+				# check if the registered value has changed
+				# during the checks => if not set it to True
+				if globalData.registered == None:
+					globalData.registered = True
+
+			else:
+				globalData.registered = False
+
+		else:
+			globalData.registered = False
+
+	except Exception as e:
+		print "Config could not be parsed."
+		print e
+		sys.exit(1)
+
+	# check if smtp is activated => generate object to send eMail alerts
+	if smtpActivated is True:
+		globalData.smtpAlert = SMTPAlert(smtpServer, smtpPort,
+			smtpFromAddr, smtpToAddr)
+	else:
+		globalData.smtpAlert = None
+
+	# initialize logging
+	logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', 
+		datefmt='%m/%d/%Y %H:%M:%S', filename=logfile, 
+		level=loglevel)
+
+	# check if sensors were found => if not exit
+	if globalData.sensors == list():
+		logging.critical("[%s]: No sensors configured. " % fileName)
+		sys.exit(1)
+
+	# generate object for the communication to the server and connect to it
+	serverComm = ServerCommunication(server, serverPort,
+		serverCertificate, username, password, globalData)
+	connectionRetries = 1
+	while 1:
+		# check if 5 unsuccessful attempts are made to connect
+		# to the server and if smtp alert is activated
+		# => send eMail alert		
+		if (globalData.smtpAlert is not None
+			and (connectionRetries % 5) == 0):
+			globalData.smtpAlert.sendCommunicationAlert(connectionRetries)
+
+		if serverComm.initializeCommunication() is True:
+			# if smtp alert is activated
+			# => send email that communication problems are solved
+			if not globalData.smtpAlert is None:
+				globalData.smtpAlert.sendCommunicationAlertClear()
+			
+			connectionRetries = 1
+			break
+		connectionRetries += 1
+
+		logging.critical("[%s]: Connecting to server failed. " % fileName
+			+ "Try again in 5 seconds.")
+		time.sleep(5)
+
+	# when connected => generate watchdog object to monitor the 
+	# server connection
+	watchdog = ConnectionWatchdog(serverComm, globalData.pingInterval,
+		globalData.smtpAlert)
+	# set thread to daemon
+	# => threads terminates when main thread terminates	
+	watchdog.daemon = True
+	watchdog.start()
+
+	# set up sensor executer and execute it
+	# (note: we will not return from the executer unless the client
+	# is terminated)
+	sensorExecuter = SensorExecuter(serverComm, globalData)
+	sensorExecuter.execute()
---
+++ sensorClientPing/lib/sensor.py
@@ -0,0 +1,264 @@
+#!/usr/bin/python2
+
+# written by sqall
+# twitter: https://twitter.com/sqall01
+# blog: http://blog.h4des.org
+# github: https://github.com/sqall01
+#
+# Licensed under the GNU Public License, version 2.
+
+import time
+import random
+import os
+import logging
+from client import AsynchronousSender
+import subprocess
+
+
+# internal class that holds the important attributes
+# for a sensor to work with (this class must be inherited from the
+# used sensor class)
+class _PollingSensor:
+
+	def __init__(self):
+		self.id = None
+		self.description = None
+		self.alertDelay = None
+		self.state = None
+		self.alertLevel = None
+		self.triggerAlert = None
+		self.triggerAlways = None
+		self.triggerState = None
+
+
+	# this function returns the current state of the sensor
+	def getState(self):
+		raise NotImplementedError("Function not implemented yet.")
+
+
+	# this function updates the state variable of the sensor
+	def updateState(self):
+		raise NotImplementedError("Function not implemented yet.")
+
+
+	# this function initializes the sensor
+	def initializeSensor(self):
+		raise NotImplementedError("Function not implemented yet.")
+
+
+# class that controls one watchdog of a challenge
+class PingWatchdogSensor(_PollingSensor):
+
+	def __init__(self):
+		_PollingSensor.__init__(self)
+
+		# used for logging
+		self.fileName = os.path.basename(__file__)
+
+		# gives the time that the process has to execute
+		self.timeout = None
+
+		# gives the interval in seconds in which the process
+		# should be checked
+		self.intervalToCheck = None
+
+		# gives the command/path that should be executed
+		self.execute = None
+
+		# gives the host of the service
+		self.host = None
+
+		# time when the process was executed
+		self.timeExecute = None
+
+		# the process itself
+		self.process = None
+
+
+	def initializeSensor(self):
+		self.timeExecute = 0.0
+
+
+	def getState(self):
+		return self.state
+
+
+	def updateState(self):
+
+		# check if a process is executed
+		# => if none no process is executed
+		if self.process is None:
+
+			# check if the interval in which the service should be checked
+			# is exceeded
+			if (int(time.time()) - self.timeExecute) > self.intervalToCheck:
+
+				logging.debug("[%s]: Executing process " % self.fileName
+							+ "'%s'." % self.description)
+				self.process = subprocess.Popen([self.execute,
+					"-c3", str(self.host)])
+				self.timeExecute = int(time.time())
+
+		# => process is still running
+		else:
+
+			# check if process is not finished yet
+			if self.process.poll() is None:
+
+				# check if process has timed out
+				if (int(time.time()) - self.timeExecute) > self.timeout:
+
+					self.state = 1
+
+					logging.error("[%s]: Process " % self.fileName
+							+ "'%s' has timed out." % self.description)
+
+					# terminate process
+					self.process.terminate()
+
+					# give the process one second to terminate
+					time.sleep(1)
+
+					# check if the process has terminated
+					# => if not kill it
+					if self.process.poll() != -15:
+						try:
+							logging.error("[%s]: Could not " % self.fileName
+							+ "terminate '%s'. Killing it." % self.description)
+
+							self.process.kill()
+						except:
+							pass
+
+					# set process to none so it can be newly started
+					# in the next state update
+					self.process = None
+
+			# process has finished
+			else:
+
+				# check if the process has exited with code 0
+				# => everything works fine
+				if self.process.poll() == 0:
+					self.state = 0
+				# process did not exited correctly
+				# => something is wrong with the ctf service
+				else:
+					self.state = 1
+
+				# set process to none so it can be newly started
+				# in the next state update
+				self.process = None
+
+
+# this class polls the sensor states and triggers alerts and state changes
+class SensorExecuter:
+
+	def __init__(self, connection, globalData):
+		self.fileName = os.path.basename(__file__)
+		self.connection = connection
+		self.sensors = globalData.sensors
+		self.globalData = globalData
+
+
+	def execute(self):
+
+		# initialize all sensors
+		for sensor in self.sensors:
+			sensor.initializeSensor()
+
+		# time on which the last full sensor states were sent
+		# to the server
+		lastFullStateSent = 0
+
+		while(1):
+
+			# check if the client is connected to the server
+			# => wait and continue loop until client is connected
+			if not self.connection.isConnected:
+				time.sleep(0.5)
+				continue
+
+			# poll all sensors and check their states
+			for sensor in self.sensors:
+
+				oldState = sensor.getState()				
+				sensor.updateState()
+				currentState = sensor.getState()
+
+				# check if the current state is the same
+				# than the already known state => continue
+				if oldState == currentState:
+					continue
+
+				# check if the current state is an alert triggering state
+				elif currentState == sensor.triggerState:
+
+					# check if the sensor triggers a sensor alert
+					# => send sensor alert to server
+					if sensor.triggerAlert:
+
+						logging.info("[%s]: Sensor alert " % self.fileName
+							+ "triggered by '%s'." % sensor.description)
+
+						asyncSenderProcess = AsynchronousSender(
+							self.connection, self.globalData)
+						# set thread to daemon
+						# => threads terminates when main thread terminates	
+						asyncSenderProcess.daemon = True
+						asyncSenderProcess.sendSensorAlert = True
+						asyncSenderProcess.sendSensorAlertSensor = sensor
+						asyncSenderProcess.start()
+
+					# if sensor does not trigger sensor alert
+					# => just send changed state to server
+					else:
+
+						logging.debug("[%s]: State " % self.fileName
+							+ "changed by '%s'." % sensor.description)
+
+						asyncSenderProcess = AsynchronousSender(
+							self.connection, self.globalData)
+						# set thread to daemon
+						# => threads terminates when main thread terminates	
+						asyncSenderProcess.daemon = True
+						asyncSenderProcess.sendStateChange = True
+						asyncSenderProcess.sendStateChangeSensor = sensor
+						asyncSenderProcess.start()
+
+				# only possible situation left => sensor changed
+				# back from triggering state to a normal state
+				# => just send changed state to server
+				else:
+
+					logging.debug("[%s]: State " % self.fileName
+						+ "changed by '%s'." % sensor.description)
+
+					asyncSenderProcess = AsynchronousSender(
+						self.connection, self.globalData)
+					# set thread to daemon
+					# => threads terminates when main thread terminates	
+					asyncSenderProcess.daemon = True
+					asyncSenderProcess.sendStateChange = True
+					asyncSenderProcess.sendStateChangeSensor = sensor
+					asyncSenderProcess.start()
+				
+			# check if the last state that was sent to the server
+			# is older than 60 seconds => send state update
+			if (time.time() - lastFullStateSent) > 60:
+
+				logging.debug("[%s]: Last state " % self.fileName
+					+ "timed out.")
+
+				asyncSenderProcess = AsynchronousSender(
+					self.connection, self.globalData)
+				# set thread to daemon
+				# => threads terminates when main thread terminates	
+				asyncSenderProcess.daemon = True
+				asyncSenderProcess.sendSensorsState = True
+				asyncSenderProcess.start()
+
+				# update time on which the full state update was sent
+				lastFullStateSent = time.time()
+				
+			time.sleep(0.5)
