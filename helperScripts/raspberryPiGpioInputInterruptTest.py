c9feef58450a948a49ac0a270e8e855fa01f1be7
Andre Pawlowski <sqall@towel.hq.h4des.org>
2014-09-11 11:58:09 +0200

    added setting for pulling the gpio up or down

--- helperScripts/raspberryPiGpioInputInterruptTest.py
+++ helperScripts/raspberryPiGpioInputInterruptTest.py
@@ -3,14 +3,19 @@ import RPi.GPIO as GPIO
 import time
 
 # NOTE: this is the actual pin number (not the number of the GPIO)
-inputPin = 26
+inputPin = 18
+
+pulledUpDown = GPIO.PUD_UP
+#pulledUpDown = GPIO.PUD_DOWN
+
+edge = GPIO.FALLING
+# edge = GPIO.RISING
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(inputPin, GPIO.IN)
+GPIO.setup(inputPin, GPIO.IN, pull_up_down=pulledUpDown)
 
 counter = 0
 while True:
-	GPIO.wait_for_edge(inputPin, GPIO.FALLING)
-	#GPIO.wait_for_edge(inputPin, GPIO.RISING)
+	GPIO.wait_for_edge(inputPin, edge)
 	counter += 1
 	print "Trigger no.: " + str(counter)
