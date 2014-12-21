9200f0eb4f9aceb026dd0edddb39cb1341067f25
Andre Pawlowski <sqall@towel.hq.h4des.org>
2014-12-21 12:51:07 +0100

    updated all alert clients to use xml for configuration

--- alertClientDbus/alertRclient.py
+++ alertClientDbus/alertRclient.py
@@ -14,9 +14,9 @@ from lib import SMTPAlert
 from lib import DbusAlert
 import logging
 import time
-import ConfigParser
 import socket
 import random
+import xml.etree.ElementTree
 
 
 # this class is a global configuration class that holds 
@@ -45,7 +45,7 @@ class GlobalData:
 
 		# path to the configuration file of the client
 		self.configFile = os.path.dirname(os.path.abspath(__file__)) \
-			+ "/config/config.conf"
+			+ "/config/config.xml"
 
 		# instance of the email alerting object
 		self.smtpAlert = None
@@ -65,44 +65,17 @@ if __name__ == '__main__':
 
 	fileName = os.path.basename(__file__)
 
-	# parse config file
+	# parse config file, get logfile configurations
+	# and initialize logging
 	try:
-		config = ConfigParser.RawConfigParser(allow_no_value=False)
+		configRoot = xml.etree.ElementTree.parse(
+			globalData.configFile).getroot()
 
-		# get config file
-		config.read([globalData.configFile])
-
-		logfile = config.get("general", "logfile")
-		server = config.get("general", "server")
-		serverPort = config.getint("general", "serverPort")
-
-		# get server certificate file and check if it does exist
-		serverCAFile = os.path.abspath(
-			config.get("general", "serverCAFile"))
-		if os.path.exists(serverCAFile) is False:
-			raise ValueError("Server CA does not exist.")
-
-		# get client certificate and keyfile (if required)
-		certificateRequired = config.getboolean("general",
-			"certificateRequired")
-		if certificateRequired is True:
-			clientCertFile = os.path.abspath(config.get("general",
-				"certificateFile"))
-			clientKeyFile = os.path.abspath(config.get("general",
-				"keyFile"))
-			if (os.path.exists(clientCertFile) is False
-				or os.path.exists(clientKeyFile) is False):
-				raise ValueError("Client certificate or key does not exist.")
-		else:
-			clientCertFile = None
-			clientKeyFile = None
-
-		# get user credentials
-		username = config.get("general", "username")
-		password = config.get("general", "password")
+		logfile = str(configRoot.find("general").find("log").attrib["file"])
 
 		# parse chosen log level
-		tempLoglevel = config.get("general", "loglevel")
+		tempLoglevel = str(
+			configRoot.find("general").find("log").attrib["level"])
 		tempLoglevel = tempLoglevel.upper()
 		if tempLoglevel == "DEBUG":
 			loglevel = logging.DEBUG
@@ -117,103 +90,159 @@ if __name__ == '__main__':
 		else:
 			raise ValueError("No valid log level in config file.")
 
+		# initialize logging
+		logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', 
+			datefmt='%m/%d/%Y %H:%M:%S', filename=logfile, 
+			level=loglevel)
+
+	except Exception as e:
+		print "Config could not be parsed."
+		print e
+		sys.exit(1)
+
+	# parse the rest of the config with initialized logging
+	try:
+
+		# parse server configurations
+		server = str(configRoot.find("general").find("server").attrib["host"])
+		serverPort = int(
+			configRoot.find("general").find("server").attrib["port"])
+
+		# get server certificate file and check if it does exist
+		serverCAFile = os.path.abspath(
+			str(configRoot.find("general").find("server").attrib["caFile"]))
+		if os.path.exists(serverCAFile) is False:
+			raise ValueError("Server CA does not exist.")
+
+		# get client certificate and keyfile (if required)
+		certificateRequired = (str(
+			configRoot.find("general").find("client").attrib[
+			"certificateRequired"]).upper()	== "TRUE")
+
+		if certificateRequired is True:
+			clientCertFile = os.path.abspath(str(
+			configRoot.find("general").find("client").attrib["certFile"]))
+			clientKeyFile = os.path.abspath(str(
+			configRoot.find("general").find("client").attrib["keyFile"]))
+			if (os.path.exists(clientCertFile) is False
+				or os.path.exists(clientKeyFile) is False):
+				raise ValueError("Client certificate or key does not exist.")
+		else:
+			clientCertFile = None
+			clientKeyFile = None
+		
+		# get user credentials
+		username = str(
+			configRoot.find("general").find("credentials").attrib["username"])
+		password = str(
+			configRoot.find("general").find("credentials").attrib["password"])
+
 		# parse smtp options if activated
-		smtpActivated = config.getboolean("smtp", "smtpActivated")
+		smtpActivated = (str(
+			configRoot.find("smtp").find("general").attrib[
+			"activated"]).upper()	== "TRUE")
 		if smtpActivated is True:
-			smtpServer = config.get("smtp", "server")
-			smtpPort = config.getint("smtp", "serverPort")
-			smtpFromAddr = config.get("smtp", "fromAddr")
-			smtpToAddr = config.get("smtp", "toAddr")
+			smtpServer = str(
+				configRoot.find("smtp").find("server").attrib["host"])
+			smtpPort = int(
+				configRoot.find("smtp").find("server").attrib["port"])
+			smtpFromAddr = str(
+				configRoot.find("smtp").find("general").attrib["fromAddr"])
+			smtpToAddr = str(
+				configRoot.find("smtp").find("general").attrib["toAddr"])
 
 		# parse all alerts
-		for section in config.sections():
-			if section.find("alert") != -1:
-				alert = DbusAlert()
+		for item in configRoot.find("alerts").iterfind("alert"):
 
-				# get dbus client settings
-				alert.triggerDelay = config.getint(section, "triggerDelay")
-				alert.displayTime = config.getint(section, "displayTime")
+			alert = DbusAlert()
 
-				# these options are needed by the server to
-				# differentiate between the registered alerts
-				alert.id = config.getint(section, "id")
-				alert.description = config.get(section, "description")
-				alert.alertLevels = map(int,
-					config.get(section, "alertLevels").split(","))
+			# get dbus client settings
+			alert.triggerDelay = int(item.find("dbus").attrib["triggerDelay"])
+			alert.displayTime = int(item.find("dbus").attrib["displayTime"])
 
-				# check if description is empty
-				if len(alert.description) == 0:
-					print "Description of alert '%s' is empty." % section
-					sys.exit(1)
+			# these options are needed by the server to
+			# differentiate between the registered alerts
+			alert.id = int(item.find("general").attrib["id"])
+			alert.description = str(item.find("general").attrib["description"])
 
-				# check if the id of the alert is unique
-				for registeredAlert in globalData.alerts:
-					if registeredAlert.id == alert.id:
-						print "Id of alert '%s'" % section \
-						+ "is already taken."
-						sys.exit(1)						
+			alert.alertLevels = list()
+			for alertLevelXml in item.iterfind("alertLevel"):
+				alert.alertLevels.append(int(alertLevelXml.text))
 
-				globalData.alerts.append(alert)
+			# check if description is empty
+			if len(alert.description) == 0:
+				raise ValueError("Description of alert '%s' is empty."
+					% section)
+
+			# check if the id of the alert is unique
+			for registeredAlert in globalData.alerts:
+				if registeredAlert.id == alert.id:
+					raise ValueError("Id of alert '%s'"
+						% section + "is already taken.")				
+
+			globalData.alerts.append(alert)
 
 		# check if the client has already registered itself at the server
 		# with the same data
 		if os.path.exists(globalData.registeredFile):
 
-			# parse registered values
-			registeredConfig = ConfigParser.RawConfigParser(
-				allow_no_value=False)
-			registeredConfig.read([globalData.registeredFile])
+			regConfigRoot = xml.etree.ElementTree.parse(
+				globalData.registeredFile).getroot()
 
-			hostname = registeredConfig.get("general", "hostname")
-			alertCount = registeredConfig.getint("general", "alertcount")
+			hostname = logfile = str(regConfigRoot.find("general").find(
+				"server").attrib["host"])			
 
 			# check if the hostname
-			if (hostname == socket.gethostname()
-				and alertCount == len(globalData.alerts)):
+			if (hostname == socket.gethostname()):
 
 				# check all alerts if their data has changed since the
 				# last registration at the server
-				for section in registeredConfig.sections():
-					if section.find("alert") != -1:			
+				alertCount = 0
+				for item in regConfigRoot.find("alerts").iterfind("alert"):
+					alertCount += 1
 
-						# get values from the file that are registered at
-						# the server
-						tempId = registeredConfig.getint(section, "id")
-						tempDescription = registeredConfig.get(section,
-							"description")
-						tempAlertLevels = map(int,
-							registeredConfig.get(section,
-							"alertLevels").split(","))
+					# get values from the file that are registered at
+					# the server
+					tempId = int(item.find("general").attrib["id"])
+					tempDescription = str(item.find("general").attrib[
+						"description"])
+					tempAlertLevels = list()
+					for alertLevelXml in item.iterfind("alertLevel"):
+						tempAlertLevels.append(int(alertLevelXml.text))
 
-						# find alert with the same id parsed from the
-						# regular config file
-						tempAlert = None
-						for alert in globalData.alerts:
-							if alert.id == tempId:
-								tempAlert = alert
-								break
-						if tempAlert == None:					
+					# find alert with the same id parsed from the
+					# regular config file
+					tempAlert = None
+					for alert in globalData.alerts:
+						if alert.id == tempId:
+							tempAlert = alert
+							break
+					if tempAlert == None:					
+						globalData.registered = False
+						break
+
+					# check if the alert data has changed since
+					# the last registration
+					if tempDescription != tempAlert.description:
+						globalData.registered = False
+						break
+
+					# check if the alert levels of the alert
+					# have changed
+					for alertLevel in tempAlert.alertLevels:
+						if not alertLevel in tempAlertLevels:
 							globalData.registered = False
 							break
-
-						# check if the alert data has changed since
-						# the last registration
-						if tempDescription != tempAlert.description:
+					for tempAlertLevel in tempAlertLevels:
+						if not tempAlertLevel in tempAlert.alertLevels:
 							globalData.registered = False
 							break
+					if globalData.registered is False:
+						break
 
-						# check if the alert levels of the alert
-						# have changed
-						for alertLevel in tempAlert.alertLevels:
-							if not alertLevel in tempAlertLevels:
-								globalData.registered = False
-								break
-						for tempAlertLevel in tempAlertLevels:
-							if not tempAlertLevel in tempAlert.alertLevels:
-								globalData.registered = False
-								break
-						if globalData.registered is False:
-							break
+				# check if the count of the alerts has changed
+				if alertCount != len(globalData.alerts):
+					globalData.registered = False
 
 				# check if the registered value has changed
 				# during the checks => if not set it to True
@@ -227,8 +256,7 @@ if __name__ == '__main__':
 			globalData.registered = False
 
 	except Exception as e:
-		print "Config could not be parsed."
-		print e
+		logging.exception("[%s]: Could not parse config." % fileName)
 		sys.exit(1)
 
 	random.seed()
