0202a77d19a9faa7b79fac80cebcc9011e78b090
Andre Pawlowski <sqall@towel.hq.h4des.org>
2014-12-23 18:01:41 +0100

    updated version to 0.221

--- sensorClientPing/alertRclient.py
+++ sensorClientPing/alertRclient.py
@@ -26,7 +26,7 @@ class GlobalData:
 	def __init__(self):
 
 		# version of the used client (and protocol)
-		self.version = 0.22
+		self.version = 0.221
 
 		# interval in which a ping should be send when
 		# no data was received/send
--- server/alertRserver.py
+++ server/alertRserver.py
@@ -29,7 +29,7 @@ class GlobalData:
 	def __init__(self):
 
 		# version of the used server (and protocol)
-		self.version = 0.22
+		self.version = 0.221
 
 		# list of all sessions that are handled by the server
 		self.serverSessions = list()
