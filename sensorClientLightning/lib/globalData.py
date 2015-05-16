874848d72d9149cc966babb7beb59be58bd96698
sqall01 <sqall@h4des.org>
2015-05-16 12:10:07 +0200

    updated version

--- sensorClientLightning/lib/globalData.py
+++ sensorClientLightning/lib/globalData.py
@@ -17,7 +17,7 @@ class GlobalData:
 	def __init__(self):
 
 		# version of the used client (and protocol)
-		self.version = 0.224
+		self.version = 0.225
 
 		# revision of the used client
 		self.rev = 0
