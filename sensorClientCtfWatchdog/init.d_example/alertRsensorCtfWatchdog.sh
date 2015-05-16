7a70413a48359f357a4c1d31a34a771ba116dff3
sqall01 <sqall@h4des.org>
2015-05-16 16:43:54 +0200

    updated init script

--- sensorClientCtfWatchdog/init.d_example/alertRsensorCtfWatchdog.sh
+++ sensorClientCtfWatchdog/init.d_example/alertRsensorCtfWatchdog.sh
@@ -20,27 +20,83 @@ DAEMON=/absolute/path/to/alertRclient.py
 NAME=alertRsensorCtfWatchdog
 PIDFILE=/var/run/$NAME.pid
 DAEMON_OPTS=""
+PATH=/sbin:/usr/sbin:/bin:/usr/bin
+# Depend on lsb-base (>= 3.2-14) to ensure that this file is present
+# and status_of_proc is working.
+. /lib/lsb/init-functions
 
-export PATH="${PATH:+$PATH:}/usr/sbin:/sbin"
+
+do_start()
+{
+	# Return
+	#   0 if daemon has been started
+	#   2 if daemon could not be started
+	start-stop-daemon --start --quiet -b --make-pidfile --pidfile $PIDFILE \
+		--chuid $USER --exec $DAEMON -- $DAEMON_OPTS \
+		|| return 2
+}
+
+
+do_stop()
+{
+	# Return
+	#   0 if daemon has been stopped
+	#   1 if daemon was already stopped
+	#   2 if daemon could not be stopped
+	#   other if a failure occurred
+	start-stop-daemon --stop --pidfile $PIDFILE --verbose \
+		--retry=TERM/30/KILL/5
+		
+	RETVAL="$?"
+	[ "$RETVAL" = 2 ] && return 2
+
+	rm -f $PIDFILE
+	return "$RETVAL"
+}
+
 
 case "$1" in
 	start)
-		echo -n "Starting daemon: "$NAME
-		start-stop-daemon --start --quiet -b --make-pidfile \
-			--pidfile $PIDFILE --chuid $USER --exec $DAEMON -- $DAEMON_OPTS
-		echo "."
+		log_daemon_msg "Starting daemon: "$NAME
+		do_start
+		case "$?" in
+			0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
+			2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
+		esac
 	;;
 	stop)
-		echo -n "Stopping daemon: "$NAME
-		start-stop-daemon --stop --pidfile $PIDFILE --verbose \
-			--retry=TERM/30/KILL/5
-		echo "."
+		log_daemon_msg "Stopping daemon: "$NAME
+		do_stop
+		case "$?" in
+			0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
+			2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
+		esac
+	;;
+	restart)
+		log_daemon_msg "Restarting daemon: "$NAME
+		do_stop
+		case "$?" in
+		  0|1)
+			do_start
+			case "$?" in
+				0) log_end_msg 0 ;;
+				1) log_end_msg 1 ;; # Old process is still running
+				*) log_end_msg 1 ;; # Failed to start
+			esac
+			;;
+		  *)
+			# Failed to stop
+			log_end_msg 1
+			;;
+		esac
+	;;
+	status)
+		status_of_proc -p $PIDFILE "$DAEMON" "$NAME" && exit 0 || exit $?
 	;;
 	*)
-		echo "Usage: "$1" {start|stop}"
+		echo "Usage: "$1" {start|restart|status|stop}"
 		exit 1
 	;;
 esac
 
 exit 0
-
