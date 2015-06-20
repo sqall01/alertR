c6432c08632581fddb7dff1c61c0f1fa9a1d0130
sqall01 <sqall@h4des.org>
2015-06-20 16:09:17 +0200

    added node and instance check

--- server/lib/userBackend.py
+++ server/lib/userBackend.py
@@ -8,6 +8,8 @@
 # Licensed under the GNU Public License, version 2.
 
 import csv
+import logging
+
 
 # internal abstract class for new user backends
 class _userBackend():
@@ -19,11 +21,21 @@ class _userBackend():
 		raise NotImplemented("Function not implemented yet.")
 
 
+	# this function checks if the node type and instance of the client
+	# is correct
+	#
+	# return True or False
+	def checkNodeTypeAndInstance(self, username, nodeType, instance):
+
+
 # user backend that uses a simple csv file
 # in the form of "username, password" per line
 class CSVBackend(_userBackend):
 
 	def __init__(self, csvLocation):
+
+		# file nme of this file (used for logging)
+		self.fileName = os.path.basename(__file__)
 
 		# stores all user credentials as a tuple of values (username, password)
 		self.userCredentials = list()
@@ -39,8 +51,23 @@ class CSVBackend(_userBackend):
 
 				username = row[0].replace(' ', '')
 				password = row[1].replace(' ', '')
+				nodeType = row[2].replace(' ', '')
+				instance = row[3].replace(' ', '')
 
-				self.userCredentials.append( (username, password) )
+				# check if username has a duplicate
+				if any(map(lambda x : x[0] == username,
+					self.userCredentials)):
+
+					logging.error("[%s]: Username '%s' already exists "
+						% (self.fileName, logString)
+						+ "in CSV file.")
+
+					continue
+
+				self.userCredentials.append( (username,
+					password,
+					nodeType,
+					instance) )
 
 
 	# this function checks if the user credentials are valid
@@ -57,6 +84,29 @@ class CSVBackend(_userBackend):
 
 			else:
 				if storedPassword == password:
+					return True
+				else:
+					return False
+
+		return False
+
+
+	# this function checks if the node type and instance of the client
+	# is correct
+	#
+	# return True or False
+	def checkNodeTypeAndInstance(self, username, nodeType, instance):
+
+		# check all usernames if the given username exist
+		# and then check the given node type and instance
+		for storedTuple in self.userCredentials:
+
+			if storedTuple[0] != username:
+				continue
+
+			else:
+				if (storedTuple[2].upper() == nodeType.upper()
+					and storedTuple[3].upper() == instance.upper()):
 					return True
 				else:
 					return False
