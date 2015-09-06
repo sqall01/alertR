da7115cc9c4aad43067e2f2fab75d56bd6763c22
sqall01 <sqall@h4des.org>
2015-09-06 10:41:30 +0200

    renamed file

--- managerClientDatabase/init.d_example/alertRmanagerDatabase.sh
+++ managerClientDatabase/init.d_example/alertRmanagerDatabase.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ### BEGIN INIT INFO
-# Provides:          alertRManagerMobile
+# Provides:          alertRmanagerDatabase
 # Required-Start:    $all
 # Should-Start:      $all
 # Required-Stop:     $remote_fs $syslog $network
@@ -17,7 +17,7 @@ USER=someUser
 # change DAEMON to the path to run the alertRclient
 DAEMON=/absolute/path/to/alertRclient.py
 
-NAME=alertRManagerMobile
+NAME=alertRmanagerDatabase
 PIDFILE=/var/run/$NAME.pid
 DAEMON_OPTS=""
 PATH=/sbin:/usr/sbin:/bin:/usr/bin
