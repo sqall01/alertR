7a698493ce7912ea834b160caf4ffd98c6e52337
sqall01 <sqall@h4des.org>
2016-11-10 11:49:58 +0100

    added license information

--- alertClientPushNotification/testPushConfiguration.py
+++ alertClientPushNotification/testPushConfiguration.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python2
 
+# written by sqall
+# twitter: https://twitter.com/sqall01
+# blog: http://blog.h4des.org
+# github: https://github.com/sqall01
+#
+# Licensed under the GNU Public License, version 2.
+
 import time
 import socket
 import ssl
