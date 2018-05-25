2c609af57db1cdd083fbc976dac5f7fb22d75a45
sqall01 <sqall@h4des.org>
2018-05-25 14:13:28 +0200

    small cheat ;)

--- alertRinstaller.py
+++ alertRinstaller.py
@@ -34,7 +34,7 @@ loglevel = logging.INFO
 # repository information
 host = "raw.githubusercontent.com"
 port = 443
-serverPath = "/sqall01/alertR/master/"
+serverPath = "/sqall01/alertR/dev/"
 
 ################ GLOBAL CONFIGURATION DATA ################
 
