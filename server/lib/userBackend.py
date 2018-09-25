f5d6df22fb424ad8644dacef4d7ee1f87e8c7f46
sqall01 <sqall@h4des.org>
2018-09-25 21:57:53 +0200

    updated CSV file format to store password as bcrypt hash

--- server/lib/userBackend.py
+++ server/lib/userBackend.py
@@ -10,19 +10,39 @@
 import csv
 import logging
 import os
+import StringIO
+import bcrypt
+import threading
 
 
-# internal abstract class for new user backends
+# Class that holds user data information.
+class UserData():
+
+	def __init__(self, username, pwhash, nodeType, instance):
+		self.username = username
+		self.pwhash = pwhash
+		self.nodeType = nodeType
+		self.instance = instance
+
+
+	def toList(self):
+		return [self.username,
+			self.pwhash,
+			self.nodeType,
+			self.instance]
+
+
+# Internal abstract class for new user backends.
 class _userBackend():
 
-	# this function checks if the user credentials are valid
+	# This function checks if the user credentials are valid
 	#
 	# return True or False
 	def areUserCredentialsValid(self, username, password):
 		raise NotImplemented("Function not implemented yet.")
 
 
-	# this function checks if the node type and instance of the client
+	# This function checks if the node type and instance of the client
 	# is correct
 	#
 	# return True or False
@@ -30,8 +50,18 @@ class _userBackend():
 		raise NotImplemented("Function not implemented yet.")
 
 
-# user backend that uses a simple csv file
-# in the form of "username, password" per line
+	# This function writes the currently cached user data back to the backend.
+	def writeUserdata(self):
+		raise NotImplemented("Function not implemented yet.")
+
+
+	# This function reads the user data from the backend and updates
+	# the current cached one.
+	def readUserdata(self):
+		raise NotImplemented("Function not implemented yet.")
+
+
+# User backend that uses a simple csv file.
 class CSVBackend(_userBackend):
 
 	def __init__(self, globalData, csvLocation):
@@ -39,81 +69,231 @@ class CSVBackend(_userBackend):
 		self.globalData = globalData
 		self.logger = self.globalData.logger
 
+		self.userDataLock = threading.BoundedSemaphore(1)
+
+		self.csvLocation = csvLocation
+
+		# CSV version in case we have to change it.
+		self.csvVersion = 1
+
 		# file nme of this file (used for logging)
 		self.fileName = os.path.basename(__file__)
 
-		# stores all user credentials as a tuple of values (username, password)
-		self.userCredentials = list()
-
-		# parse csv file and store all user credentials
-		with open(csvLocation, 'rb') as csvFile:
-			csvReader = csv.reader(csvFile, quoting=csv.QUOTE_ALL)
-			for row in csvReader:
-				if len(row) != 4:
-					continue
-				if row[0].find('#') != -1:
-					continue
-
-				username = row[0].replace(' ', '')
-				password = row[1].replace(' ', '')
-				nodeType = row[2].replace(' ', '')
-				instance = row[3].replace(' ', '')
-
-				# check if username has a duplicate
-				if any(map(lambda x : x[0] == username,
-					self.userCredentials)):
-
-					self.logger.error("[%s]: Username '%s' already exists "
-						% (self.fileName, logString)
-						+ "in CSV file.")
-
-					continue
-
-				self.userCredentials.append( (username,
-					password,
-					nodeType,
-					instance) )
+		self.readUserdata()
 
 
-	# this function checks if the user credentials are valid
+	# Internal function that acquires the lock.
+	def _acquireLock(self):
+		self.logger.debug("[%s]: Acquire lock." % self.fileName)
+		self.userDataLock.acquire()
+
+
+	# Internal function that releases the lock.
+	def _releaseLock(self):
+		self.logger.debug("[%s]: Release lock." % self.fileName)
+		self.userDataLock.release()
+
+
+	# Parses the data in csv version 0.
+	# Does not acquire or release the lock.
+	def _parseVersion0(self, csvData):
+		for row in csvData:
+			if row[0].find('#') != -1:
+				continue
+			if len(row) != 4:
+				continue
+
+			username = row[0].replace(' ', '')
+			password = row[1].replace(' ', '')
+			nodeType = row[2].replace(' ', '')
+			instance = row[3].replace(' ', '')
+
+			# Check if username has a duplicate.
+			if any(map(lambda x : x.username == username,
+				self.userCredentials)):
+
+				self.logger.error("[%s]: Username '%s' already exists "
+					% (self.fileName, username)
+					+ "in CSV file.")
+
+				continue
+
+			pwhash = bcrypt.hashpw(password, bcrypt.gensalt())
+			userData = UserData(username, pwhash, nodeType, instance)
+			self.userCredentials.append(userData)
+
+
+	# Parses the data in csv version 1.
+	# Does not acquire or release the lock.
+	def _parseVersion1(self, csvData):
+		for row in csvData:
+			if row[0].find('#') != -1:
+				continue
+			if len(row) != 4:
+				continue
+
+			username = row[0].replace(' ', '')
+			pwhash = row[1].replace(' ', '')
+			nodeType = row[2].replace(' ', '')
+			instance = row[3].replace(' ', '')
+
+			# Check if username has a duplicate.
+			if any(map(lambda x : x.username == username,
+				self.userCredentials)):
+
+				self.logger.error("[%s]: Username '%s' already exists "
+					% (self.fileName, username)
+					+ "in CSV file.")
+
+				continue
+
+			userData = UserData(username, pwhash, nodeType, instance)
+			self.userCredentials.append(userData)
+
+
+	# Extracts the version from the file.
+	def _getVersion(self, csvData):
+		version = 0
+		for row in csvData:
+			if len(row) == 1 and row[0].startswith("Version:"):
+				try:
+					version = int(row[0][8:])
+				except Exception as e:
+					self.logger.exception("[%s]: Unable to extract version of "
+						% self.fileName
+						+ "CSV file")
+			break
+		return version
+
+
+	# This function checks if the user credentials are valid.
 	#
 	# return True or False
 	def areUserCredentialsValid(self, username, password):
 
-		# check all usernames if the given username exist
-		# and then if the password is the correct one
-		for storedTuple in self.userCredentials:
+		self._acquireLock()
 
-			if storedTuple[0] != username:
+		# Check all usernames if the given username exist
+		# and then if the password is the correct one.
+		for userData in self.userCredentials:
+
+			if userData.username != username:
 				continue
 
 			else:
-				if storedTuple[1] == password:
+				if bcrypt.checkpw(password, userData.pwhash):
+					self._releaseLock()
 					return True
 				else:
+					self._releaseLock()
 					return False
 
+		self._releaseLock()
 		return False
 
 
-	# this function checks if the node type and instance of the client
-	# is correct
+	# This function checks if the node type and instance of the client
+	# is correct.
 	#
 	# return True or False
 	def checkNodeTypeAndInstance(self, username, nodeType, instance):
 
+		self._acquireLock()
+
 		# check all usernames if the given username exist
 		# and then check the given node type and instance
-		for storedTuple in self.userCredentials:
+		for userData in self.userCredentials:
 
-			if storedTuple[0] != username:
+			if userData.username != username:
 				continue
 
 			else:
-				if (storedTuple[2].upper() == nodeType.upper()
-					and storedTuple[3].upper() == instance.upper()):
+				if (userData.nodeType.upper() == nodeType.upper()
+					and userData.instance.upper() == instance.upper()):
+
+					self._releaseLock()
 					return True
+
 				else:
+					self._releaseLock()
 					return False
 
+		self._releaseLock()
 		return False
+
+
+	# This function reads the user data from the backend and updates
+	# the current cached one.
+	def readUserdata(self):
+
+		self.logger.info("[%s]: Reading user data from CSV file."
+			% self.fileName)
+
+		self._acquireLock()
+
+		# Stores all user credentials as an object.
+		self.userCredentials = list()
+
+		# Parse csv file and store all user credentials.
+		csvData = []
+		with open(self.csvLocation, 'rb') as csvFile:
+			csvReader = csv.reader(csvFile, quoting=csv.QUOTE_ALL)
+			csvData = list(csvReader)
+
+		# Extract version of the csv file.
+		version = self._getVersion(csvData)
+
+		# Parse the csv file according to the version.
+		if version == 0:
+			self._parseVersion0(csvData)
+		elif version == 1:
+			self._parseVersion1(csvData)
+		else:
+			self.logger.error("[%s]: Do not know how to parse CSV file with "
+				% self.fileName
+				+ "version %d."
+				% version)
+
+		self._releaseLock()
+
+		# Write user data back if we have changed the csv file format.
+		if version != self.csvVersion:
+			self.writeUserdata()
+
+
+	# This function writes the currently cached user data back to the backend.
+	def writeUserdata(self):
+
+		self.logger.info("[%s]: Writing user data to CSV file."
+			% self.fileName)
+
+		self._acquireLock()
+
+		# Set version of the current csv format.
+		fileData = "Version:%d\n" % self.csvVersion
+
+		# Add comments for the csv file.
+		fileData += "#\n"
+		fileData += "# Characters not allowed for"
+		fileData += " usage are '#', ',' and '\"'.\n"
+		fileData += "# The values are \"username, bcrypt(password), node type,"
+		fileData += " client instance\" per line.\n"
+		fileData += "# Note: whitespaces will be removed during parsing.\n"
+
+		# Create csv data for the file.
+		output = StringIO.StringIO()
+		csvWriter = csv.writer(output,
+			delimiter=",",
+			quoting=csv.QUOTE_ALL)
+		for userData in self.userCredentials:
+			csvWriter.writerow(userData.toList())
+
+		# Add csv data to the final file data.
+		fileData += output.getvalue()
+		output.close()
+
+		# Write final file.
+		with open(self.csvLocation, 'wb') as csvFile:
+			csvFile.write(fileData)
+
+		self._releaseLock()
