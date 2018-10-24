4917e84a13d7328804d564754c19d93cffd2f2c0
sqall01 <sqall@h4des.org>
2018-10-24 17:30:08 +0200

    typo

--- server/lib/userBackend.py
+++ server/lib/userBackend.py
@@ -344,7 +344,7 @@ class CSVBackend(_userBackend):
 		fileData += "# The values are \"username, bcrypt(password), node type,"
 		fileData += " client instance\" per line.\n"
 		fileData += "# Note: whitespaces will be removed during parsing.\n"
-		fileData += "# Please do not modify this file directly and use " 
+		fileData += "# Please do not modify this file directly and use\n" 
 		fileData += "# 'manageUsers.py' instead."
 
 		# Create csv data for the file.
