9cb5547e7df8da5b36a37c68e56ae47ae9a6d6c6
sqall01 <sqall@h4des.org>
2020-01-26 14:56:33 +0100

    migration 2 to 3

--- server/lib/globalData.py
+++ server/lib/globalData.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
 # written by sqall
 # twitter: https://twitter.com/sqall01
-# blog: http://blog.h4des.org
+# blog: https://h4des.org
 # github: https://github.com/sqall01
 #
 # Licensed under the GNU Affero General Public License, version 3.
@@ -10,21 +10,22 @@
 import os
 import threading
 import ssl
+from typing import Optional
 
 
 # Class implements an iterator that iterates over a copy of the
 # server sessions.
 class ServerSessionsIterator(object):
 
-    def __init__(self, serverSessions):
+    def __init__(self, server_sessions):
         self.idx = 0
-        self.serverSessions = list(serverSessions)
+        self.server_sessions = list(server_sessions)
 
     def __next__(self):
-        if self.idx >= len(self.serverSessions):
+        if self.idx >= len(self.server_sessions):
             raise StopIteration
         self.idx += 1
-        return self.serverSessions[self.idx - 1]
+        return self.server_sessions[self.idx - 1]
 
     def next(self):
         return self.__next__()
@@ -34,20 +35,20 @@ class ServerSessionsIterator(object):
 class ServerSessions(object):
 
     def __init__(self):
-        self._serverSessions = list()
-        self._serverSessionsLock = threading.Lock()
+        self._server_sessions = list()
+        self._server_sessions_lock = threading.Lock()
 
-    def append(self, serverSession):
-        with self._serverSessionsLock:
-            self._serverSessions.append(serverSession)
+    def append(self, server_session):
+        with self._server_sessions_lock:
+            self._server_sessions.append(server_session)
 
-    def remove(self, serverSession):
-        with self._serverSessionsLock:
-            self._serverSessions.remove(serverSession)
+    def remove(self, server_session):
+        with self._server_sessions_lock:
+            self._server_sessions.remove(server_session)
 
     def __iter__(self):
-        with self._serverSessionsLock:
-            return ServerSessionsIterator(self._serverSessions)
+        with self._server_sessions_lock:
+            return ServerSessionsIterator(self._server_sessions)
 
 
 # this class is a global configuration class that holds
@@ -57,19 +58,19 @@ class GlobalData:
     def __init__(self):
 
         # version of the used server (and protocol)
-        self.version = 0.504
+        self.version = 0.505  # type: float
 
         # revision of the used server
-        self.rev = 1
+        self.rev = 0  # type: int
 
         # Used database layout version.
-        self.dbVersion = 1
+        self.dbVersion = 1  # type: int
 
         # name of this server
-        self.name = "AlertR Server"
+        self.name = "AlertR Server"  # type: str
 
         # the instance of this server
-        self.instance = "server"
+        self.instance = "server"  # type: str
 
         # instance of the storage backend
         self.storage = None
@@ -103,32 +104,29 @@ class GlobalData:
         self.configCheckInterval = 60.0
 
         # path to the configuration file of the client
-        self.configFile = os.path.dirname(os.path.abspath(__file__)) \
-            + "/../config/config.xml"
+        self.configFile = os.path.dirname(os.path.abspath(__file__)) + "/../config/config.xml"
 
         # path to the csv user credentials file (if csv is used as backend)
-        self.userBackendCsvFile = os.path.dirname(os.path.abspath(__file__)) \
-            + "/../config/users.csv"
+        self.userBackendCsvFile = os.path.dirname(os.path.abspath(__file__)) + "/../config/users.csv"
 
         # path to the sqlite database file (if sqlite is used as backend)
-        self.storageBackendSqliteFile = os.path.dirname(os.path.abspath(
-            __file__)) + "/../config/database.db"
+        self.storageBackendSqliteFile = os.path.dirname(os.path.abspath(__file__)) + "/../config/database.db"
 
         # How often the alertR server should try to connect to the
         # MySQL server when the connection establishment fails.
         self.storageBackendMysqlRetries = 5
 
         # location of the certifiacte file
-        self.serverCertFile = None
+        self.serverCertFile = None  # type: Optional[str]
 
         # location of the key file
-        self.serverKeyFile = None
+        self.serverKeyFile = None  # type: Optional[str]
 
         # do clients authenticate themselves via certificates
-        self.useClientCertificates = None
+        self.useClientCertificates = None  # type: Optional[bool]
 
         # path to CA that is used to authenticate clients
-        self.clientCAFile = None
+        self.clientCAFile = None  # type: Optional[str]
 
         # Get TLS/SSL setting.
         try:
@@ -136,8 +134,7 @@ class GlobalData:
         except AttributeError:
             self.sslProtocol = ssl.PROTOCOL_SSLv23
         self.sslOptions = ssl.OP_ALL
-        self.sslCiphers = "HIGH:!aNULL:!eNULL:!EXPORT" \
-            + ":!CAMELLIA:!DES:!MD5:!PSK:!RC4"
+        self.sslCiphers = "HIGH:!aNULL:!eNULL:!EXPORT:!CAMELLIA:!DES:!MD5:!PSK:!RC4"
 
         # a list of all alert levels that are configured on this server
         self.alertLevels = list()
@@ -160,15 +157,15 @@ class GlobalData:
 
         # Information concerning logging instances.
         self.logger = None
-        self.logdir = None
-        self.loglevel = None
+        self.logdir = None  # type: Optional[str]
+        self.loglevel = None  # type: Optional[str]
 
         # Number of failed attempts for version information fetching
         # from the update repository before generating a notification.
         self.updateMaxFails = 10
 
         # Unique id of this server (is also the username of this server).
-        self.uniqueID = None
+        self.uniqueID = None  # type: Optional[str]
 
         # List of all sessions that are handled by the server.
         self.serverSessions = ServerSessions()
--- server/lib/survey.py
+++ server/lib/survey.py
@@ -1,28 +1,30 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 
 # written by sqall
 # twitter: https://twitter.com/sqall01
-# blog: http://blog.h4des.org
+# blog: https://h4des.org
 # github: https://github.com/sqall01
 #
 # Licensed under the GNU Affero General Public License, version 3.
 
-from localObjects import ErrorCodes
-import socket
-import ssl
+from .localObjects import ErrorCodes
+from .globalData import GlobalData
 import time
 import threading
 import os
 import json
-import logging
 import requests
 
 
-# this class participates in the alertR survey (if it is activated)
+# this class participates in the AlertR survey (if it is activated)
 class SurveyExecuter(threading.Thread):
 
-    def __init__(self, updateActivated, updateServer, updateLocation,
-        globalData):
+    def __init__(self,
+                 updateActivated: bool,
+                 updateServer: str,
+                 updateLocation: str,
+                 globalData: GlobalData,
+                 timeout: float = 20.0):
         threading.Thread.__init__(self)
 
         # used for logging
@@ -38,76 +40,66 @@ class SurveyExecuter(threading.Thread):
         self.updateActivated = updateActivated
         self.updateServer = updateServer
         self.updateLocation = updateLocation
+        self.timeout = timeout
 
         # fixed values for survey
-        self.surveyInterval = 604800 # week
+        self.surveyInterval = 604800  # week
         self.host = "https://alertr.de/submit.php"
 
+    def sendSurveyData(self) -> bool:
+        """
+        Gather and send survey data.
 
-    # gather and send survey data
-    def sendSurveyData(self):
-
-        self.logger.info("[%s]: Starting to send survey data."
-                % self.fileName)
+        :return: Success or Failure
+        """
+        self.logger.info("[%s]: Starting to send survey data." % self.fileName)
 
         # gather survey data
         surveyNodes = self.storage.getSurveyData()
         if surveyNodes is None:
-
-            self.logger.error("[%s]: Did not get any survey data from "
-                % self.fileName
-                + "the database.")
+            self.logger.error("[%s]: Did not get any survey data from the database." % self.fileName)
             return False
+
         surveyData = dict()
-        surveyData["type"] = "survey";
+        surveyData["type"] = "survey"
         surveyData["nodes"] = surveyNodes
 
         surveyUpdate = dict()
         surveyUpdate["activated"] = self.updateActivated
         surveyUpdate["server"] = self.updateServer
         surveyUpdate["location"] = self.updateLocation
-        surveyData["update"] = surveyUpdate
 
+        surveyData["update"] = surveyUpdate
         surveyData["uniqueID"] = self.storage.getUniqueID()
 
         # Send survey data to server.
-        r = None
         try:
             payload = {"data": json.dumps(surveyData)}
-            r = requests.post(self.host, verify=True, data=payload)
+            r = requests.post(self.host,
+                              verify=True,
+                              data=payload,
+                              timeout=self.timeout)
 
             # Check if server responded correctly.
             if r.status_code != 200:
-                raise ValueError("Server response code not 200 (was %d)."
-                    % r.status_code)
+                raise ValueError("Server response code not 200 (was %d)." % r.status_code)
 
             data = r.json()
             if data["code"] != ErrorCodes.NO_ERROR:
                 msg = ""
                 if "msg" in data.keys():
                     msg = data["msg"]
-                raise ValueError("Server responded with error code '%d' and "
-                    % data["code"]
-                    + "message: '%s'."
-                    % msg)
+                raise ValueError("Server responded with error code '%d' and message: '%s'." % (data["code"], msg))
 
         except Exception as e:
-            self.logger.exception("[%s]: Sending survey data failed."
-                % self.fileName)
-
+            self.logger.exception("[%s]: Sending survey data failed." % self.fileName)
             return False
 
-        self.logger.info("[%s]: Survey data successfully sent."
-                % self.fileName)
-
+        self.logger.info("[%s]: Survey data successfully sent." % self.fileName)
         return True
 
-
     def run(self):
-
         while True:
-
             # sleep for the interval before participating to the survey
             time.sleep(self.surveyInterval)
-
             self.sendSurveyData()
