f215eeefab925fe20b01f41e40c749860c10cd29
sqall01 <sqall@h4des.org>
2020-05-31 10:47:39 +0200

    moved files to shared code

--- managerClientTemplate/lib/globalData/__init__.py
+++ managerClientTemplate/lib/globalData/__init__.py
@@ -1,10 +1,1 @@
-#!/usr/bin/python3
-
-# written by sqall
-# twitter: https://twitter.com/sqall01
-# blog: https://h4des.org
-# github: https://github.com/sqall01
-#
-# Licensed under the GNU Affero General Public License, version 3.
-
-from .globalData import GlobalData
+../../../shared_code/clients_manager/lib/globalData/__init__.py
