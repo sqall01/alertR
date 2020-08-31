8f118c072228f9bc2c49099c2a697a84f46ab33e
sqall01 <sqall@h4des.org>
2020-08-31 19:35:54 +0200

    WIP: return instrumentation results via promise

--- server/lib/alert/instrumentation.py
+++ server/lib/alert/instrumentation.py
@@ -13,7 +13,72 @@ import os
 import subprocess
 import threading
 import time
+from typing import Optional
 from ..localObjects import AlertLevel, SensorAlert
+
+
+class PromiseState:
+    SUCCESS = 1
+    FAILED = 2
+    PENDING = 3
+
+
+class InstrumentationPromise:
+
+    def __init__(self,
+                 alert_level: AlertLevel,
+                 sensor_alert: SensorAlert):
+        self._alert_level = alert_level
+        self._orig_sensor_alert = sensor_alert
+        self._new_sensor_alert = None
+        self._finished_event = threading.Event()
+        self._finished_event.clear()
+        self._state = PromiseState.PENDING
+        self._creation_time = int(time.time())
+
+    @property
+    def alert_level(self):
+        return self._alert_level
+
+    @property
+    def new_sensor_alert(self):
+        return self._new_sensor_alert
+
+    @property
+    def orig_sensor_alert(self):
+        return self._orig_sensor_alert
+
+    def is_finished(self,
+                    blocking: bool = False,
+                    timeout: Optional[float] = None) -> bool:
+        if blocking:
+            self._finished_event.wait()
+
+        if timeout is not None:
+            self._finished_event.wait(timeout)
+
+        return self._state != PromiseState.PENDING
+
+    def set_failed(self):
+        self._state = PromiseState.FAILED
+        self._finished_event.set()
+
+    def set_new_sensor_alert(self, sensor_alert: SensorAlert):
+        self._new_sensor_alert = sensor_alert
+
+    def set_success(self):
+        self._state = PromiseState.SUCCESS
+        self._finished_event.set()
+
+    def was_successful(self):
+        if self._state == PromiseState.SUCCESS:
+            return True
+
+        elif self._state == PromiseState.FAILED:
+            return False
+
+        else:
+            raise ValueError("Instrumentation not finished.")
 
 
 class Instrumentation:
@@ -23,7 +88,8 @@ class Instrumentation:
         self._logger = logger
         self._alert_level = alert_level
         self._sensor_alert = sensor_alert
-        self._thread = None
+        self._promise = None  # type: Optional[InstrumentationPromise]
+        self._thread = None  # type: Optional[threading.Thread]
 
     def _execute(self):
         """
@@ -41,7 +107,7 @@ class Instrumentation:
 
         except Exception:
             self._logger.exception("[%s]: Executing instrumentation for Alert Level '%d' failed."
-                              % (self._log_tag, self._alert_level.level))
+                                   % (self._log_tag, self._alert_level.level))
             self._logger.error("[%s]: Command was: %s" % (self._log_tag, " ".join(temp_execute)))
             return
 
@@ -62,10 +128,10 @@ class Instrumentation:
             if exit_code != -15:
                 try:
                     self._logger.error("[%s]: Could not terminate instrumentation for Alert Level '%d'. Killing it."
-                                  % (self._log_tag, self._alert_level.level))
+                                       % (self._log_tag, self._alert_level.level))
 
                     process.kill()
-                except:
+                except Exception:
                     pass
 
             return
@@ -74,6 +140,7 @@ class Instrumentation:
         output, err = process.communicate()
         output = output.decode("ascii")
         err = err.decode("ascii")
+
         if exit_code == 0:
             if not self._process_output(output):
                 self._logger.error("[%s]: Unable to process output from instrumentation for Alert Level '%d'."
@@ -140,9 +207,9 @@ class Instrumentation:
             if self._sensor_alert.dataType != new_sensor_alert.dataType:
                 raise ValueError("dataType not allowed to change")
 
-            # TODO store and signal new sensor alert object
-
-            # TODO test cases
+            # Set result.
+            self._promise.set_new_sensor_alert(new_sensor_alert)
+            self._promise.set_success()
 
         except Exception:
             self._logger.exception("[%s]: Could not parse received output from from instrumentation "
@@ -153,10 +220,17 @@ class Instrumentation:
 
         return True
 
-    def execute(self):
+    def execute(self) -> InstrumentationPromise:
         """
         Execute instrumentation in a non-blocking way.
+        :return: promise which contains the results after instrumentation finished execution.
         """
+        self._promise = InstrumentationPromise(self._alert_level, self._sensor_alert)
+
         self._thread = threading.Thread(target=self._execute)
         self._thread.daemon = True
         self._thread.start()
+
+        return self._promise
+
+# TODO test cases
